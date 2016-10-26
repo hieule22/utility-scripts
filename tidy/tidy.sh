9dc975dad394c963458a4f38c9f755acef300612
Hieu Le <trunghieule94@gmail.com>
2016-10-26 10:36:56 -0500

    Add options for tidy

--- tidy/tidy.sh
+++ tidy/tidy.sh
@@ -3,27 +3,60 @@
 # Press d to delete junk files and any other key to skip.
 # Copyright 2016 Hieu Le
 
+# Echo all command in debugging mode.
+DEBUG=false
+if [ $DEBUG = true ]; then
+    set -x
+fi
+
+# DEFAULT PROGRAM OPTIONS.
 # Regular expression of the file names to be deleted.
 REGEX="*~"
-# Key command to perform deletion.
+# Root directory to scan.
+DIRECTORY=.
+# Other options.
+OPTS=""
+
+# PROGRAM CONSTANTS.
+# Keyboard input to perform deletion.
 DELETE="d"
 
-echo "Press d to delete and anything else to skip!"
-junks=( $(find . -name $REGEX) )
-echo "Found" ${#junks[@]} "file(s)."
-
-for file in ${junks[@]}
+# Parse command line arguments.
+for ARG in "$@"
 do
-    read -n1 -r -s -p $file opt
-    echo  # Begin on new line.
-    if [ "$opt" == $DELETE ]; then
-	echo "Deleting" $file
-	rm $file
-    else
-	echo "Skipping" $file
-    fi
+    case $ARG in
+	-d=*|-directory=*)
+	    DIRECTORY="${ARG#*=}"
+	    shift # Skip past argument=value.
+	    ;;
+	-regex=*)
+	    REGEX="${ARG#*=}"
+	    shift
+	    ;;
+	-recursive=no) # Stop recursing into subdirectories.
+	    OPTS+="-prune "
+	    shift
+	    ;;
+	*)
+	    # Unknown option.
+	    echo "Unknown option: $ARG"
+	;;
+    esac
 done
 
+echo "Press $DELETE to delete and anything else to skip!"
 
+JUNKS=( $(find $DIRECTORY -name $REGEX $OPTS) )
+echo "Found" ${#JUNKS[@]} "file(s)."
 
-
+for FILE in ${JUNKS[@]}
+do
+    read -n1 -r -s -p $FILE OPT
+    echo  # Begin on new line.
+    if [ "$OPT" == $DELETE ]; then
+	echo "Deleting" $FILE
+	rm $FILE
+    else
+	echo "Skipping" $FILE
+    fi
+done
